820d8192464fb5973c2c1540460e4fc8b332df23
Enmanuel Feliz Espinal <enmanuelfeliz106@gmail.com>
2021-01-13 10:38:26 -0600

    Trabajos component terminado

--- portafolio/src/app/trabajos/trabajos.component.ts
+++ portafolio/src/app/trabajos/trabajos.component.ts
@@ -9,9 +9,14 @@ import { AnimacionesService } from '../services/animaciones.service';
 })
 export class TrabajosComponent implements OnInit {
 
+  infoProyecto = '';
+  nombreProyecto = '';
+
   constructor(private animaciones: AnimacionesService) { }
 
   ngOnInit(): void {
+
+    $('.modal').hide();
 
     /**La navegacion comienza aqui, por eso aqui se aplican los stilos y scripts para todos los componentes  */
 
@@ -43,5 +48,39 @@ export class TrabajosComponent implements OnInit {
   cerrarMensaje(){
     $('header.mensaje').fadeOut(1000);
   }
+
+  abrirModal(proyecto: string){
+    switch (proyecto){
+      case 'yamelitas':
+        this.nombreProyecto = 'Yamelitas';
+        this.infoProyecto = `Página web en proceso de desarrollo, comencé este proyecto web el 02/01/2021. Yamelitas es una tienda virtual de productos para el cuidado de la piel y el cabello. Orientado mayormente a un público femenino jóven y liderado por dos jovenes emprendedoras. `;
+        $('.yamelitas .modal').fadeIn(500);
+        $('.mealsplan .modal').fadeOut(500);
+        break;
+
+      case 'mealsplan':
+        this.nombreProyecto = 'Meals Plan';
+        this.infoProyecto = `Esta aplicación híbrida es uno de mis proyectos personales, el cual está disponible para android. Meals Plan es un calendarizador de comidas para amantes de la cocina y el fitness.`;
+        $('.mealsplan .modal').fadeIn(500);
+        $('.yamelitas .modal').fadeOut(500);
+        break;
+
+      default:
+        this.infoProyecto = '';
+
+    }
+
+    
+    
+    
+  }
+
+
+  cerrarModal(){
+    $('.modal').fadeOut(500);
+
+  }
+
+
 
 }
