6374aea2e627f4f7d1b64227afaa4ff307b9f1f6
Enmanuel Feliz Espinal <enmanuelfeliz106@gmail.com>
2021-03-15 21:44:01 -0400

    Removiendo archivos innecesarios y arreglando margenes entre proyectos

--- portafolio/src/environments/environment.prod.ts
+++ portafolio/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDIR3UTFE0irFL_cJJYichwRvDd5Ucb_iI",
+    authDomain: "enmanuelfelizportfolio.firebaseapp.com",
+    projectId: "enmanuelfelizportfolio",
+    storageBucket: "enmanuelfelizportfolio.appspot.com",
+    messagingSenderId: "274804282070",
+    appId: "1:274804282070:web:71d23dea58cdd157586f77",
+    measurementId: "G-TW2F5KQG7S"
+  }
 };
